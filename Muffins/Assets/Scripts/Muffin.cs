b26ff340e65cd4173769d29f29ff25a05d2afd48
Sonny Li <sonnynomnom@gmail.com>
2020-11-16 14:33:00 -0500

    11/16 animation

--- Muffins/Assets/Scripts/Muffin.cs
+++ Muffins/Assets/Scripts/Muffin.cs
@@ -13,31 +13,50 @@ public class Muffin : MonoBehaviour
     public RectTransform[] spinLights;
     public float spinSpeed = 20;
 
+    public Vector2 minimumXandY;
+    public Vector2 maximumXandY;
+
+    public GameObject muffinTextReward;
+
+    public int textRewardDuration = 10;
+
+    public RectTransform myRectTransform;
+
     public void OnMuffinClick()
     {
         AddMuffins(muffinsPerClick);
         UpdateTextUI();
+        CreateTextReward();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        print("Start called");
+        print(message: "Start called");
+
+        myRectTransform = GetComponent<RectTransform>();
     }
-    
+
     // Update is called once per frame
     void Update()
     {
         // Rotation the spinlight every frame
         // spinLights.Rotate(0, 0, spinSpeed * Time.deltaTime);
-        AnimateSpinLights();    
+        AnimateSpinLights();
     }
 
     public void AnimateSpinLights()
     {
-        for(int index = 0; index < spinLights.Length; index = index + 1)
+        for (int index = 0; index < spinLights.Length; index = index + 1)
         {
-            spinLights[index].Rotate(0, 0, spinSpeed * Time.deltaTime);
+            float currentSpeed = spinSpeed;
+
+            if (index % 2 == 0)
+            {
+                currentSpeed = currentSpeed * 2;
+            }
+
+            spinLights[index].Rotate(xAngle: 0, yAngle: 0, zAngle: currentSpeed * Time.deltaTime);
         }
     }
 
@@ -49,6 +68,31 @@ public class Muffin : MonoBehaviour
     public void AddMuffins(int muffinsToAdd)
     {
         numberOfEarnedMuffins = numberOfEarnedMuffins + muffinsToAdd;
+    }
+
+    public void CreateTextReward()
+    {
+        Vector2 pos = GetRandomPosition();
+        GameObject newTextReward = Instantiate(muffinTextReward, myRectTransform);
+
+        newTextReward.transform.localPosition = pos;
+
+        MuffinRewardAnimationInfo animInfo = new MuffinRewardAnimationInfo();
+
+        animInfo.transform = newTextReward.GetComponent<RectTransform>();
+        animInfo.textField = newTextReward.GetComponent<TMP_Text>();
+
+
+    }
+    
+    public Vector2 GetRandomPosition()
+    {
+
+        float x = Random.Range(minimumXandY.x, maximumXandY.x);
+        float y = Random.Range(minimumXandY.y, maximumXandY.y);
+
+        return new Vector2(x,y);
+
     }
 
 }
---
+++ Muffins/Assets/Scripts/MuffinRewardAnimationInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MuffinRewardAnimationInfo : MonoBehaviour
+{
+    public RectTransform transform;
+    public TMP_Text textField;
+
+    public float ellapsedTime = 0;
+
+    public void Destroy()
+    {
+        Destroy(transform.gameObject);
+    }
+}
