812376dc7061dbf82997718d0133450d8ebd9414
Sonny Li <sonnynomnom@gmail.com>
2020-11-30 14:33:22 -0500

    nov 30th

--- Muffins/Assets/Scripts/GameManager.cs
+++ Muffins/Assets/Scripts/GameManager.cs
@@ -7,6 +7,12 @@ public class GameManager : MonoBehaviour
 
     public int numberOfEarnedMuffins = 0;
 
+    public void Start()
+    {
+        StartCoroutine(SaveDataPeriodically());
+        LoadMyData();
+    }
+
     public void AddMuffins(int muffinsToAdd)
     {
         numberOfEarnedMuffins = numberOfEarnedMuffins + muffinsToAdd;
@@ -16,23 +22,41 @@ public class GameManager : MonoBehaviour
     {
         // transforming C# object into text
         string dataAsString = JsonUtility.ToJson(myData);
-        
+
         // sets the text to a "progress" file
         PlayerPrefs.SetString("progress", dataAsString);
         // Debug.Log(dataAsString);
-        
+
         // saves the file in the disk
         PlayerPrefs.Save();
 
     }
 
-    public void OnApplicationPause(bool pauseStatus)
+    private IEnumerator SaveDataPeriodically()
+    {
+        while (true == true)
+        {
+            yield return new WaitForSeconds(2);
+            SaveCurrentData();
+        }
+    }
+
+    public void LoadMyData()
+    {
+        if (PlayerPrefs.HasKey("progress"))
+        {
+            string dataAsString = PlayerPrefs.GetString("progress");
+            SaveData savedData = (JsonUtility.FromJson(dataAsString, typeof(SaveData)) as SaveData);
+        }
+    }
+
+    public void SaveCurrentData()
     {
         SaveData currentData = new SaveData();
         currentData.numberOfMuffins = numberOfEarnedMuffins;
         currentData.name = "Jerry";
 
         SaveMyData(currentData);
-    }
 
+    }
 }
